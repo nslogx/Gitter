3f932d918150d504fde43c0cfc960273fe398724
jianke <huangjianke00@gmail.com>
2019-02-20 11:22:31 +0800

    新增获取 OpenID 云函数

---
+++ cloud/functions/openid/index.js
@@ -0,0 +1,16 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
